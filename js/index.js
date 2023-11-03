ded067f776cf55180f24c6e9b7dc4b33cdffc015
Feline Huhn <feline.huhn@gmx.de>
2023-11-03 15:37:26 +0100

    resolve challenge

--- js/index.js
+++ js/index.js
@@ -41,6 +41,14 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.dev/api/people");
+  const data = await response.json();
+  const starWarsPeople = data.results;
+  console.log(data);
+
+  starWarsPeople.forEach((person) => {
+    const card = Card(person);
+    renderElement(card);
+  });
 }
